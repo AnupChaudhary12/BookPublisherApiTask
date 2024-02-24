712562cc89f21e0d1666d6253ab762d2e34de875
Anup Chaudhary <57917543+AnupChaudhary12@users.noreply.github.com>
2024-02-24 16:08:52 +0545

    Added Book And Publisher Model

---
+++ BookPublisher.Api/Entities/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPublisher.Domain.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = default!;
+        public string Author { get; set; } = default!;
+        public string ISBN { get; set; } = default!;
+        public DateTime PublishedDate { get; set; }
+
+        public int PublisherId { get; set; }
+        public Publisher? Publisher { get; set; } 
+    }
+}
---
+++ BookPublisher.Api/Entities/Publisher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPublisher.Domain.Entities
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Address { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public ICollection<Book> Books { get; set; } 
+    }
+}
